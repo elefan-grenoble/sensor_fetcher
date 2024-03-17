cc416d52d74fdb7cbef7927b87b1a893000d3acd
Albin PETIT <albin.petit@gmail.com>
2024-03-17 09:40:22 +0100

    Add log_level variable

--- src/sensor_fetcher.py
+++ src/sensor_fetcher.py
@@ -6,12 +6,15 @@ from database_manager import DatabaseManager
 from mqtt_connector import MqttConnector
 
 if __name__ == "__main__":
-    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=log_fmt)
-
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
+    # Set the logging level using a variable from .env
+    log_level_str = os.getenv("LOG_LEVEL", "DEBUG")
+    log_level = getattr(logging, log_level_str.upper())
+    # Define the log format string and set the logging level
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=log_level, format=log_fmt)
     # Initialize the database manager
     db_manager = DatabaseManager()
     # Create the table if it doesn't exist

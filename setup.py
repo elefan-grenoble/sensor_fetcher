5030840ef67b826aff319e9b6c8bec599d7d8e2d
Albin PETIT <albin.petit@gmail.com>
2024-03-17 09:49:20 +0100

    Bumped version number to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.2',
     description='Récupération de données de capteur envoyées sur un broker MQTT',
     author='Albin Petit',
     license='MIT',
